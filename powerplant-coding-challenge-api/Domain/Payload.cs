c52d9309cba298802f848f02b31f942b5859dc41
Florian <florian.morel@student.vinci.be>
2021-03-11 16:44:23 +0100

    Initialize domain's skeleton after first reading of the exercize

---
+++ powerplant-coding-challenge-api/Domain/Payload.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Payload
+    {
+        public Load EnergyAmount { get; set; }
+
+        public List<Fuel> Fuels { get; set; }
+
+        public List<Powerplant> Powerplant { get; set; }
+
+    }
+}
