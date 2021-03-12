8708992cce4414770bfa22ee3c4203ef4058c008
Florian <floppy007_flo@hotmail.com>
2021-03-12 13:15:31 +0100

    start by implementing a sort by powerplant type

--- powerplant-coding-challenge-api/BusinessLayer/PowerCalculator.cs
+++ powerplant-coding-challenge-api/BusinessLayer/PowerCalculator.cs
@@ -1,17 +1,31 @@
 ﻿using BusinessLayer.interfaces;
 using Domain;
-using System;
+using Domain.Enum;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BusinessLayer
 {
     public class PowerCalculator : IPowerCalculator
     {
+        Dictionary<string, List<Powerplant>> standardPowerplantOrderDictionnary = new Dictionary<string, List<Powerplant>> {
+                { PowerPlantType.WINDTURBINE, new List<Powerplant>() },
+                { PowerPlantType.GASFIRED, new List<Powerplant>() },
+                { PowerPlantType.TURBOJET, new List<Powerplant>() },
+            };
 
+        //The priority order depending of the type is hardcoded I should find a better way 
         public List<Powerplant> SortPowerPlant(List<Powerplant> powerplants)
         {
-            Dictionary<string, Powerplant> powerplantDictionnary = new Dictionary<string, Powerplant>();
-            return new List<Powerplant>();
+            standardPowerplantOrderDictionnary[PowerPlantType.WINDTURBINE].AddRange(powerplants.Where(p => p.Type == PowerPlantType.WINDTURBINE).ToList());
+            standardPowerplantOrderDictionnary[PowerPlantType.GASFIRED].AddRange(powerplants.Where(p => p.Type == PowerPlantType.GASFIRED).ToList());
+            standardPowerplantOrderDictionnary[PowerPlantType.TURBOJET].AddRange(powerplants.Where(p => p.Type == PowerPlantType.TURBOJET).ToList());
+
+            var orderedPowerplants = new List<Powerplant>();
+            foreach (var p in standardPowerplantOrderDictionnary.Values)
+                orderedPowerplants.AddRange(p);
+
+            return orderedPowerplants;
         }
     }
 }
--- powerplant-coding-challenge-api/TestProject/PowerplantCalculationTest.cs
+++ powerplant-coding-challenge-api/TestProject/PowerplantCalculationTest.cs
@@ -1,23 +1,49 @@
+using BusinessLayer;
+using Domain;
+using Domain.Enum;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace TestProject
 {
     [TestClass]
     public class PowerplantCalculationTest
     {
+
+        PowerCalculator calculator;
+
         [TestInitialize]
-        public void Initialize() { }
+        public void Initialize()
+        {
+            calculator = new PowerCalculator();
+        }
 
         [TestMethod]
-        public void Foo()
+        public void PowerplantAreCorrectlyOrder()
         {
             //
-            var foo = true;
+            var input = new List<Powerplant>
+            {
+                new Powerplant{Type = PowerPlantType.TURBOJET},
+                new Powerplant{Type = PowerPlantType.WINDTURBINE},
+                new Powerplant{Type = PowerPlantType.GASFIRED},
+            };
+
+            var expected = new List<Powerplant>
+            {
+                new Powerplant{Type = PowerPlantType.WINDTURBINE},
+                new Powerplant{Type = PowerPlantType.GASFIRED},
+                new Powerplant{Type = PowerPlantType.TURBOJET},
+            };
+            //
+            var result = calculator.SortPowerPlant(input);
 
             //
-
-            //
-            Assert.IsTrue(foo) ;
+            Assert.AreEqual(expected.Count, result.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
         }
     }
 }
