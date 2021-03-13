2da8f152f466158c84a13299b37824cf0c41ab5b
Florian <floppy007_flo@hotmail.com>
2021-03-13 17:46:04 +0100

    add some test for wind powerplant

---
+++ powerplant-coding-challenge-api/TestProject/WindProducerTest.cs
@@ -0,0 +1,90 @@
+﻿using BusinessLayer;
+using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.TestUtils;
+
+namespace TestProject
+{
+    [TestClass]
+    public class WindProducerTest
+    {
+        private WindProducer windProducer;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var powerplant = DummyObjectFactory.GetDummyWindPowerplant();
+            var fuel = DummyObjectFactory.GetDummyFuel();
+            windProducer = new WindProducer(powerplant, fuel);
+        }
+
+        [TestMethod]
+        public void ReturnProductionPlanWith0PowerWhenLoadIs0()
+        {
+            //
+            var load = 0;
+
+            //
+            var result = windProducer.ReduceLoad(ref load);
+
+            //
+            Assert.IsTrue(result.Power == 0);
+            Assert.IsTrue(load == 0);
+        }
+
+
+        [TestMethod]
+        public void ReturnProductionPlanWithCorrectPowerWhenLoadIs0()
+        {
+            //
+            var powerplant = DummyObjectFactory.GetDummyWindPowerplant();
+            powerplant.Pmax = 10;
+            windProducer.Powerplant = powerplant;
+            var load = 5;
+
+            //
+            var result = windProducer.ReduceLoad(ref load);
+
+            //
+            Assert.IsTrue(result.Power == 5);
+            Assert.IsTrue(load == 0);
+        }
+
+        [TestMethod]
+        public void ReturnProductionPlanWithBiggerPowerWhenLoadIs0()
+        {
+            //
+            var powerplant = DummyObjectFactory.GetDummyWindPowerplant();
+            powerplant.Pmax = 10;
+            windProducer.Powerplant = powerplant;
+            var load = 15;
+
+            //
+            var result = windProducer.ReduceLoad(ref load);
+
+            //
+            Assert.IsTrue(result.Power == 10);
+            Assert.IsTrue(load == 5);
+        }
+       
+        [TestMethod]
+        public void ReturnProductionPlanWithPowerWhenLoadIs0()
+        {
+            //
+            var powerplant = DummyObjectFactory.GetDummyWindPowerplant();
+            powerplant.Pmax = 10;
+            windProducer.Powerplant = powerplant;
+            var load = 0;
+
+            //
+            var result = windProducer.ReduceLoad(ref load);
+
+            //
+            Assert.IsTrue(result.Power == 0);
+            Assert.IsTrue(load == 0);
+        }
+    }
+}
